de2556d053d018fa2f4d0965b7c982d1929c08e0
Pepperrs <Pepper-n@pschonline.de>
2015-03-10 16:56:50 +0000

    added TestcasesController and most according actions.

---
+++ app/controllers/testcases_controller.rb
@@ -0,0 +1,25 @@
+class TestcasesController < ApplicationController
+  unloadable
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
