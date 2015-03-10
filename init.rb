b3977b767ca81276257497eee519941e556629b8
Pepperrs <Pepper-n@pschonline.de>
2015-03-10 12:21:53 +0100

    init.rb has been updated with actual informations.

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :red_jenkins do
   name 'Red Jenkins plugin'
   author 'Author name'
-  description 'This is a plugin for Redmine'
+  description 'This plugin includes Jenkins in Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://www.isp.uni-luebeck.de'
+  author_url 'https://github.com/Pepperrs/red_jenkins'
 end
