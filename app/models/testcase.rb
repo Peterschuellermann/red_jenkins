7babb0d993525099bfe68670e0df48c861f63115
c-pid <me@c-pid.net>
2015-03-12 15:10:37 +0100

    Added first simple validation to Testcase

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -1,3 +1,9 @@
 class Testcase < ActiveRecord::Base
-  unloadable
+    validates :name, 		presence: 	true,
+							length:		{in:5..50},
+							uniqueness: { scope: :path}
+						
+	validates :description, presence:	true
+	validates :path,		presence:	true,
+							length:		{minimum: 50}
 end
