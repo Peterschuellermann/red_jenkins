1a80d66d3b25bb265a0f7610a928399e98b9b860
c-pid <me@c-pid.net>
2015-03-14 13:38:28 +0100

    Implementation of testcase Controller

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -1,25 +1,57 @@
 class TestcasesController < ApplicationController
-  unloadable
-
-
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+ 	def index
+	@testcases = Testcase.all
+	end
+	
+	def show
+	#leer
+	end
+	
+	def new
+		@testcase = Testcase.new
+	end
+	
+	def edit
+		@testcase = Testcase.find(params[:id])
+	end
+	
+	def create
+		testcase_input = testcase_params
+		testcase_input["time_last_run"] = DateTime.now
+		testcase_input["state"] = "PASSED"
+		testcase_input["test_type"] = 1
+		@testcase = Testcase.new(testcase_input)
+ 
+		if @testcase.save
+			redirect_to testcases_path
+		else
+			render 'new'
+		end
+	end
+	
+	def update
+		@testcase = Testcase.find(params[:id])
+		testcase_input = testcase_params
+		testcase_input["time_last_run"] = DateTime.now
+		testcase_input["state"] = "PASSED"
+		testcase_input["test_type"] = 1
+ 
+		if @testcase.update(testcase_input)
+			redirect_to testcases_path
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@testcase = Testcase.find(params[:id])
+		@testcase.destroy
+ 
+		redirect_to testcases_path
+	end
+	
+	private
+		def testcase_params
+			params.require(:testcase).permit(:name, :description, :path)
+		end
 end
