433fa3ec4794b7b96e324c76f5a44e2aec4d93aa
René Schönfelder <schoenfr@isp.uni-luebeck.de>
2015-03-17 12:04:17 +0100

    made treestructure for testcases a helper function

--- app/controllers/testcases_controller.rb
+++ app/controllers/testcases_controller.rb
@@ -1,58 +1,58 @@
 class TestcasesController < ApplicationController
- 	def index
-  @project = Project.find(params[:project_id])
-	@testcases = Testcase.all
-	end
-	
-	def show
-	#leer
-	end
-	
-	def new
-		@testcase = Testcase.new
-	end
-	
-	def edit
-		@testcase = Testcase.find(params[:id])
-	end
-	
-	def create
-		testcase_input = testcase_params
-		testcase_input["time_last_run"] = DateTime.now
-		testcase_input["state"] = "UNKNOWN"
-		testcase_input["test_type"] = "MANUAL"
-		@testcase = Testcase.new(testcase_input)
- 
-		if @testcase.save
-			redirect_to testcases_path
-		else
-			render 'new'
-		end
-	end
-	
-	def update
-		@testcase = Testcase.find(params[:id])
-		testcase_input = testcase_params
-		testcase_input["time_last_run"] = DateTime.now
-		testcase_input["test_type"] = "MANUAL"
- 
-		if @testcase.update(testcase_input)
-			redirect_to testcases_path
-		else
-			render 'edit'
-		end
-	end
-	
-	def destroy
-		@testcase = Testcase.find(params[:id])
-		@testcase.destroy
- 
-		redirect_to testcases_path
-	end
-	
-	private
-		def testcase_params
-			params.require(:testcase).permit(:name, :description, :path)
-		end
+    def index
+        @project = Project.find(params[:project_id])
+        @testcases = Testcase.all
+    end
+
+    def show
+        #leer
+    end
+
+    def new
+        @testcase = Testcase.new
+    end
+
+    def edit
+        @testcase = Testcase.find(params[:id])
+    end
+
+    def create
+        testcase_input = testcase_params
+        testcase_input["time_last_run"] = DateTime.now
+        testcase_input["state"] = "UNKNOWN"
+        testcase_input["test_type"] = "MANUAL"
+        @testcase = Testcase.new(testcase_input)
+
+        if @testcase.save
+            redirect_to testcases_path
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @testcase = Testcase.find(params[:id])
+        testcase_input = testcase_params
+        testcase_input["time_last_run"] = DateTime.now
+        testcase_input["test_type"] = "MANUAL"
+
+        if @testcase.update(testcase_input)
+            redirect_to testcases_path
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @testcase = Testcase.find(params[:id])
+        @testcase.destroy
+
+        redirect_to testcases_path
+    end
+
+    private
+    def testcase_params
+        params.require(:testcase).permit(:name, :description, :path)
+    end
 
 end
--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -1,12 +1,12 @@
 class Testcase < ActiveRecord::Base
     validates :name, 		presence: 	true,
-							length:		{in:5..50},
-							uniqueness: { scope: :path}
-						
-	validates :path,		presence:	true
-							
-	attr_accessible :name, :description, :time_last_run, 
-					:path, :status, :test_type
-					
-	has_many :issues, through: :testcaseissuerelations
+        length:		{in:5..50},
+        uniqueness: { scope: :path}
+
+    validates :path,		presence:	true
+
+    attr_accessible :name, :description, :time_last_run,
+        :path, :status, :test_type
+
+    has_many :issues, through: :testcaseissuerelations
 end
