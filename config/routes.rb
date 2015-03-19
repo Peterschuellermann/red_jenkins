44f92b249af10e665fbcdcd0739aa471b7bddf6a
René Schönfelder <schoenfr@isp.uni-luebeck.de>
2015-03-19 17:10:26 +0100

    toggle button now toggle testcase status via ajax call to controller function

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ resources :projects do
 end
 
 get '/issues/:issue_id/progress', :to => 'testcases#progress'
+get '/projects/:project_id/testcases/:id/toggle', :to => 'testcases#toggle'
 get '/projects/:project_id/jenkins_update', :to => 'jenkinshandler#updatecases'
